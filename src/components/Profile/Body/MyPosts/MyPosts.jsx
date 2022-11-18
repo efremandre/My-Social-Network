a27b5a1ffac913ea2d5c608a21038ceebd9f4f68
Andrey Eferemov <samo.dur.andre@gmail.com>
2022-11-18 07:50:08 +0300

    feat: add textarea

--- src/components/Profile/Body/MyPosts/MyPosts.jsx
+++ src/components/Profile/Body/MyPosts/MyPosts.jsx
@@ -4,8 +4,17 @@ import classes from './MyPosts.module.css';
 const MyPosts = () => {
 	return (
 		<div className={classes.my_post}>
-			<h3>Wall</h3>
-		</div>
+			<h3 className={classes.title} >Wall</h3>
+			<form className={classes.form} action="post">
+				<div className={classes.textarea_block}>
+					<textarea className={classes.textarea} name="post" rows="3" placeholder='Enter new post...'></textarea>
+					<input className={classes.button_reset} type="reset" value="x" />
+				</div>
+				<div className={classes.button_row}>
+					<input className={`${classes.button} ${classes.button_submit}`} type="button" value="Submit" />
+				</div>
+			</form>
+		</div >
 	)
 }
 

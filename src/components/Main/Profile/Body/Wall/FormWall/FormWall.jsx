0b6a8ec467b9bef74199068474e9d3e1019f3cc4
Andrey Eferemov <samo.dur.andre@gmail.com>
2022-12-07 00:32:30 +0300

    add is onClick from button in profile

--- src/components/Main/Profile/Body/Wall/FormWall/FormWall.jsx
+++ src/components/Main/Profile/Body/Wall/FormWall/FormWall.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import classes from './FormWall.module.css';
 
 const FormWall = () => {
+
+	const textArea = React.createRef();
+
+	const addPost = () => {
+		let textPost = textArea.current;
+		alert(textPost.value);
+		textPost.value = '';
+	}
+
 	return (
 		<form className={classes.form} action="post">
 			<div className={classes.textarea_block}>
-				<textarea className={classes.textarea} name="post" rows="3" placeholder='Enter new post...'></textarea>
+				<textarea className={classes.textarea} name="post" rows="3" placeholder='Enter new post...' ref={textArea} ></textarea>
 				<input className={classes.button_reset} type="reset" value="x" />
 			</div>
 			<div className={classes.button_row}>
-				<input className={`${classes.button} ${classes.button_submit}`} type="button" value="Submit" />
+				<input className={`${classes.button} ${classes.button_submit}`} type="button" value="Submit" onClick={addPost} />
 			</div>
 		</form>
 	)
